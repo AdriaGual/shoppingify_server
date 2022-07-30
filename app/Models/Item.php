004580dcfce7b4e0264740a306f273608b411dd4
AdriaGual <u1933571@campus.udg.edu>
2022-07-31 00:58:44 +0200

    first commit

---
+++ app/Models/Item.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    public $table = 'item';
+    use HasFactory;
+
+    /**
+     * Get the category that owns the comment.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+}
