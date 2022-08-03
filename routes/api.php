0901d0a5a250c8ed9a11def243eda734c02e27a0
AdriaGual <u1933571@campus.udg.edu>
2022-08-03 14:10:55 +0200

    add Cors handling

--- routes/api.php
+++ routes/api.php
@@ -20,25 +20,28 @@ use App\Http\Controllers\ListaController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware(['cors'])->group(function () {
+    Route::get('/categories/find_by_name', [CategoryController::class, 'find_by_name']);
+    Route::get('/items/find_by_name', [ItemController::class, 'find_by_name']);
+    Route::get('/lists/find_by_name', [ListaController::class, 'find_by_name']);
 
-Route::get('/categories/find_by_name', [CategoryController::class, 'find_by_name']);
-Route::get('/items/find_by_name', [ItemController::class, 'find_by_name']);
-Route::get('/lists/find_by_name', [ListaController::class, 'find_by_name']);
+    Route::get('/lists/find_items/{id}', [ListaController::class, 'find_items']);
 
-Route::get('/lists/find_items/{id}', [ListaController::class, 'find_items']);
+    Route::get('/categories/get_items', [CategoryController::class, 'get_items']);
 
-Route::get('/categories/get_items', [CategoryController::class, 'get_items']);
+    Route::put('/lists/update_list_items', [ListaController::class, 'update_list_items']);
 
-Route::put('/lists/update_list_items', [ListaController::class, 'update_list_items']);
+    Route::post('/lists/set_active_list/{list_id}',[ListaController::class, 'set_active_list']);
 
-Route::post('/lists/set_active_list/{list_id}',[ListaController::class, 'set_active_list']);
+    Route::post('/lists/add_item_to_list/{item_id}/{list_id}',[ListaController::class, 'add_item_to_list']);
 
-Route::post('/lists/add_item_to_list/{item_id}/{list_id}',[ListaController::class, 'add_item_to_list']);
+    Route::resource('items',ItemController::class);
 
-Route::resource('items',ItemController::class);
+    Route::resource('categories',CategoryController::class);
 
-Route::resource('categories',CategoryController::class);
+    Route::resource('lists',ListaController::class);
 
-Route::resource('lists',ListaController::class);
+});
+
 
 
